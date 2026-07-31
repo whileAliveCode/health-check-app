c8f1f5e0b2cd97a4428ed464415c91d738b71486
whileAliveCode <jverma.dev01@gmail.com>
2026-07-31 05:47:42 +0530

    Add /health endpoint

---
+++ src/main/java/com/whileAliveCode/health_check_app/HealthCheckAppApplication.java
@@ -0,0 +1,13 @@
+package com.whileAliveCode.health_check_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCheckAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCheckAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/whileAliveCode/health_check_app/HealthController.java
@@ -0,0 +1,12 @@
+package com.whileAliveCode.health_check_app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String health(){
+        return "OK";
+    }
+}
